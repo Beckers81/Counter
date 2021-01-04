3ad1181f3c470240ed1bdabb5bcc1fb8c6906ce9
Becky Henry <beckyhenry@Beckys-MacBook-Pro.local>
2021-01-04 11:36:25 -0700

    started new counter app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {render} from 'react-dom';
+const App = () => {
+    const [count, setCount] = useState(0);
+    return(
+        <React.Fragment>
+            <h1>Becky Style App</h1>
+            <span>
+            <h2>Current Count: {count}</h2>
+            </span>
+            <button onClick={() => setCount(count + 1)}>Increment Count</button>
+            <button onClick={() => setCount(count - 1)}>Decrement Count</button>
+            <button onclick={() => setCount(0)}>Clear Count</button>
+        </React.Fragment>
+    );
+}
+
+render (<App/>, document.getElementById('root'));
